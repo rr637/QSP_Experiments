75290f343c4a698807afe419dc6253e52ee64e76
Your Name <your_netid@cornell.edu>
2024-01-25 13:08:19 -0500

    search exp'

--- search_cpp_demo.py
+++ search_cpp_demo.py
@@ -10,7 +10,7 @@ import csv
 #Compile cpp code to executable, if it hasn't been compiled already.
 # Or, if there's an issue with running the executable, try removing it and
 # recompiling
-params = {'protein': 'homo_sapien', 'num_protein': 1, 'num_qubits':10, 'Avg_CX': 0, 'Avg_Runtime(s)': 0}
+params = {'protein': 'homo_sapien', 'num_protein': 20, 'num_qubits':10, 'Avg_CX': 0, 'Avg_Runtime(s)': 0}
 n = 10
 CX_search_counts = []
 Runtimes_search = []
@@ -110,49 +110,49 @@ with open(csv_file_path, 'w', newline='') as csvfile:
 
 
 
-#Compile cpp code to executable, if it hasn't been compiled already.
-# Or, if there's an issue with running the executable, try removing it and
-# recompiling
-if "search_cpp" not in os.listdir():
-    os.system("./build_search")
+# #Compile cpp code to executable, if it hasn't been compiled already.
+# # Or, if there's an issue with running the executable, try removing it and
+# # recompiling
+# if "search_cpp" not in os.listdir():
+#     os.system("./build_search")
 
-n = 10
+# n = 10
 
-start_time = time.time()
+# start_time = time.time()
 
-#Same command as running the ISA executable. To change the number of VQC layers,
-# go into cpp/vqc_main.cpp and modify the variable "num_layers" under main()
-state_file = 'States/state0.txt'
-os.system(f"./search_cpp output.txt {state_file} {n} 0.95")
+# #Same command as running the ISA executable. To change the number of VQC layers,
+# # go into cpp/vqc_main.cpp and modify the variable "num_layers" under main()
+# state_file = 'States/state0.txt'
+# os.system(f"./search_cpp output.txt {state_file} {n} 0.95")
 
-#Read the gates from the output file, same as for ISA
-gate_sequence = []
-with open("output.txt") as f:
-    while True:
-        line = f.readline()
-        if len(line) == 0: break
-        if len(line) < 3: continue
-        [gate_type, a1, a2] = line.split()
-        if gate_type == "rx":
-            gate_sequence.append(qc.Gate.RX(int(a1), float(a2), n))
-        elif gate_type == "ry":
-            gate_sequence.append(qc.Gate.RY(int(a1), float(a2), n))
-        elif gate_type == "rz":
-            gate_sequence.append(qc.Gate.RZ(int(a1), float(a2), n))
-        elif gate_type == "cx" or gate_type == "xc":
-            gate_sequence.append(qc.Gate.CX(int(a1), int(a2), n))
-        else: raise
+# #Read the gates from the output file, same as for ISA
+# gate_sequence = []
+# with open("output.txt") as f:
+#     while True:
+#         line = f.readline()
+#         if len(line) == 0: break
+#         if len(line) < 3: continue
+#         [gate_type, a1, a2] = line.split()
+#         if gate_type == "rx":
+#             gate_sequence.append(qc.Gate.RX(int(a1), float(a2), n))
+#         elif gate_type == "ry":
+#             gate_sequence.append(qc.Gate.RY(int(a1), float(a2), n))
+#         elif gate_type == "rz":
+#             gate_sequence.append(qc.Gate.RZ(int(a1), float(a2), n))
+#         elif gate_type == "cx" or gate_type == "xc":
+#             gate_sequence.append(qc.Gate.CX(int(a1), int(a2), n))
+#         else: raise
 
-end_time = time.time()
-runtime = end_time - start_time
-CX_count = 0
-for gate in gate_sequence:
-    # print(gate.to_string())
-    if gate.gate_type == "cx" or gate.gate_type == "xc":
-        CX_count += 1
+# end_time = time.time()
+# runtime = end_time - start_time
+# CX_count = 0
+# for gate in gate_sequence:
+#     # print(gate.to_string())
+#     if gate.gate_type == "cx" or gate.gate_type == "xc":
+#         CX_count += 1
 
         
-print(f"Number of Gates: {len(gate_sequence)}")
-print(f"CX Count: {CX_count}")
-print(f"Script runtime: {runtime} seconds")
-print(state_file)
+# print(f"Number of Gates: {len(gate_sequence)}")
+# print(f"CX Count: {CX_count}")
+# print(f"Script runtime: {runtime} seconds")
+# print(state_file)
